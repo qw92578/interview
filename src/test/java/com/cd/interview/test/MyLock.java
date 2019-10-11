4627d8750a7f3c18fff4f86a5044369c791a1e18
fanggq <qw92578@gmail.com>
2019-10-11 09:11:38 +0800

    test up

---
+++ src/test/java/com/cd/interview/test/MyLock.java
@@ -0,0 +1,13 @@
+package com.cd.interview.test;
+
+/**
+ * @Package: com.cd.interview.test
+ * @ClassName: MyLock
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/10/9 11:23
+ */
+public class MyLock {
+    public static Object objA;
+    public static Object objB;
+}
